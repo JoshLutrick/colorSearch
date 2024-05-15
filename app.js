f37e77348bfb18d21c6e8bd86a5ebef8328d5978
JoshLutrick <joshslutrick@gmail.com>
2024-05-14 18:46:18 -0700

    checks to see if the input matches up with object color array

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ const btnSubmit = document.getElementById("submitBtn")
 const txtSearch = document.getElementById("searchText");
 
 
+
+
+const colorSearch = {
+    colors: ['orange', 'green', 'blue', 'red', 'sky blue', 'purple', 'cherry red'],
+
+}
+
+function checkInput(){
+    const txtInputValue = txtSearch.value
+    for(let color of colorSearch.colors){
+        if(txtInputValue === color){
+            return true;
+        } else if(!txtInputValue === color){
+            return false;
+        }
+    }
+}
+
+
+btnSubmit.addEventListener('click', checkInput);
